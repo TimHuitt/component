12dc4bfe4b5f98f7ee317a993401563e6823f82f
Tim Huitt <timhuitt@gmail.com>
2023-12-17 17:42:35 -0700

    update

--- Componenter/index.jsx
+++ Componenter/index.jsx
@@ -53,21 +53,6 @@ const Componenter = ({ exclusions }) => {
     return null;
   };
   
-  // base instruction for AI
-  const baseRequest = {
-    Task: `Create an element/component to replace the pre element with id 'content-creator'. Your returned code will be placed within a React component's return() statement. Follow the guidelines EXTREMELY STRICTLY`,
-    Requirement: `Find the pre element with id'content-creator' in the baseHtml and provide ONLY the element that fits within it's place`,
-    Guidelines: [
-      `ONLY respond with the CODE you create. Do not provide any additional details or notes about your code, decision making, final product, etc., before OR after the code statement.`,
-      `ONLY respond with code that belongs inside the React fragment.`,
-      `Do not include anything other than the necessary elements. This means NO comments, NO opening/closing text explaining the element, etc.`,
-      `NEVER include top-level elements like root or App. All elements should fit within the jsx fragments.`,
-      `Ensure seamless integration into the existing code and only use react-based styling, i.e., style={{display:'flex'}}.`,
-      `If using colors, guarantee visibility of font colors with selected background colors.`,
-      `In case of inability to meet constraints or need to provide data that does not fit within the return() statement, ALWAYS respond with an error message.`,
-      `ALWAYS Adhere STRICTLY to the provided guidelines and constraints.`,
-    ],
-  }
 
   // user instruction for AI
   const userRequest = `create a dropdown with 3 options`
